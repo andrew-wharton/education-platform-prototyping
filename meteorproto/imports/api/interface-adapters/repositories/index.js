3dd4095fd0ce34c4bdf354e25d569f2f1f1c83ba
andrew <andrew@whartonlabs.com>
2017-02-08 21:50:31 +1100

    added index files for exporting classes

---
+++ meteorproto/imports/api/interface-adapters/repositories/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by andrew on 7/2/17.
+ */
+
+export * from './AssessmentItemTemplateRepository.js';
+export * from './AssessmentTemplateRepository.js';
+export * from './LessonRepository.js';
---
+++ meteorproto/imports/api/use-cases/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by andrew on 7/2/17.
+ */
+
+export * from './LessonUseCases.js';
+export * from './AssessmentTemplateUseCases.js';
