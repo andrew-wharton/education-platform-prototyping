f10467474bd7b3d97fcaac691b9cfd4f313c319c
andrew <andrew@whartonlabs.com>
2017-02-24 20:47:12 +1100

    added initial schema for Lesson data

--- meteorproto/imports/api/lesson/LessonMongoCollection.js
+++ meteorproto/imports/api/lesson/LessonMongoCollection.js
@@ -2,14 +2,23 @@
  * @fileOverview Application side schema as MongoDB is schemaless
  */
 var LessonSchema = new SimpleSchema({
-
+  startAt: {
+    type: Date
+  },
+  endAt: {
+    type: Date,
+    optional: true
+  },
+  program: {
+    type: [String]
+  }
 });
 
 
 var LessonMongoCollection = new Mongo.Collection("lessons");
 
 // TODO add schema
-//LessonCollection.attachSchema(LessonSchema);
+LessonMongoCollection.attachSchema(LessonSchema);
 
 Meteor.startup(function () {
   if(Meteor.isServer) {
